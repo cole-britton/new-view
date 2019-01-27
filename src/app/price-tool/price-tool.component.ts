7275816f8f209ce9040571fef01ccd424678052b
Cole Britton <brxttxn@coles-mbp.lan>
2019-01-26 21:03:42 -0800

    now asking for total sqft instead of average

--- src/app/price-tool/price-tool.component.ts
+++ src/app/price-tool/price-tool.component.ts
@@ -9,46 +9,58 @@ import { Router } from '@angular/router';
 export class PriceToolComponent implements OnInit {
 
   totalPrice: string = "$0";
-  averagesqft: number;
+  totalSqft: number;
   units: number;
   upload: boolean = false;
   totalAmenitySqft: number;
   spaces: number;
   amenities: boolean = false;
+  presentPricePerUnit: boolean = false;
+  costPerUnit: string = "$0";
 
 
   constructor(private router: Router) { }
 
-  goHome(){
+  goHome() {
     this.router.navigate(['']);
   }
 
   calcTotal() {
     var tempTotal = 0;
     var costPerSqFt = .125;
+    var startingCostPerSqFt = .25
     var costPerSqFtAmenity = .20;
+    var discountPerUnitSub50 = .00255102;
     var discountPerExtraUnit = .0001285714;
     var uploadCost = 5;
     var uploadDenominator = 2.65;
+    this.presentPricePerUnit = false;
 
-    if (this.averagesqft != null && this.units != null && this.averagesqft > 0 && this.units > 0) {
-      if (this.units <= 50) {
-        tempTotal = costPerSqFt * this.averagesqft * this.units;
+    if (this.totalSqft != null && this.units != null && this.totalSqft > 0 && this.units > 0) {
+      var averageSqft = this.totalSqft/this.units;
+      if (this.units <= 49) {
+        var sub50Discount = startingCostPerSqFt - (discountPerUnitSub50 * this.units);
+        tempTotal = sub50Discount * averageSqft * this.units;
       } else {
-        tempTotal = costPerSqFt * this.averagesqft * 50;
+        tempTotal = costPerSqFt * averageSqft * 50;
         var remainingUnits = (this.units - 50);
         var discount = remainingUnits * discountPerExtraUnit;
         var newCostPerSqFt = costPerSqFt - discount;
         if (remainingUnits > 350) {
           newCostPerSqFt = .08;
         }
-        tempTotal = tempTotal + (remainingUnits * newCostPerSqFt * this.averagesqft);
+        tempTotal = tempTotal + (remainingUnits * newCostPerSqFt * averageSqft);
       }
 
+
       if (this.upload) {
-        var uploadBonus = (uploadCost/uploadDenominator) * (this.averagesqft/1000);
+        var uploadBonus = (uploadCost / uploadDenominator) * (averageSqft / 1000);
         tempTotal = tempTotal + ((uploadBonus + uploadCost) * this.units);
       }
+
+      var costPerUnitNum = Math.ceil(tempTotal / this.units);
+      this.costPerUnit = "$" + this.numberWithCommas(costPerUnitNum);
+      this.presentPricePerUnit = true;
 
       if (this.totalAmenitySqft != null && this.spaces != null && this.amenities) {
         if (this.spaces > 0) {
@@ -57,7 +69,6 @@ export class PriceToolComponent implements OnInit {
       }
 
     }
-
 
 
     tempTotal = Math.floor(tempTotal); //round to nearest dollar

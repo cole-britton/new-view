7dfc7589e67b563af3b53f2c63322e1884da012b
Cole Britton <brxttxn@Coles-MacBook-Pro.local>
2019-04-02 11:45:36 -0700

    corrected some home page issues, started PB

---
+++ src/app/package-builder/new-view-package.ts
@@ -0,0 +1,4 @@
+export interface INewViewPackage {
+    id: number;
+    selectedFeatures: any[];
+}
--- src/app/package-builder/package-builder.component.ts
+++ src/app/package-builder/package-builder.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { ActivatedRoute } from '@angular/router';
+import { INewViewPackage } from './new-view-package';
 
 @Component({
   selector: 'app-package-builder',
@@ -10,14 +11,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PackageBuilderComponent implements OnInit {
 
-  packageIdFromRoute: number;
+  currentPackage: INewViewPackage;
 
   constructor(private router: Router, private db: AngularFireDatabase, private ar: ActivatedRoute) { }
 
   ngOnInit() {
-    this.packageIdFromRoute=this.ar.snapshot.params['id'];
+    this.setCurrentPackage(this.ar.snapshot.params['id'])
   }
   
 
-  
+  setCurrentPackage(packageId: number) {
+    this.currentPackage.id = packageId;
+  }
 }

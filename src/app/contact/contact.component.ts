113c39b97d81187d7baaf1121d6012b70f429f41
Cole Britton <brxttxn@Coles-MacBook-Pro.local>
2019-04-20 18:01:17 -0700

    finished contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IContact } from './contact-package';
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+  submitted = false;
+
+  name = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  message = new FormControl('', [Validators.required]);
+
+  contactPackage: IContact = {
+    name: "",
+    email: "",
+    message: ""
+  }
+
+  constructor(private formBuilder: FormBuilder, private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.maxLength(500)]]
+    });
+  }
+
+
+  getErrorMessage(formControl: FormControl) {
+    return formControl.hasError('required') ? 'You must enter a value' :
+      formControl.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  sendClicked() {
+    if (this.contactForm.invalid) {
+      return;
+    }
+
+    this.submitMessage(this.contactForm.value);
+  }
+
+  submitMessage(cf: {}) {
+    this.db.list('contactUs').push(cf)
+      .then(_ => {
+        this.submitted = true;
+      })
   }
 
 }

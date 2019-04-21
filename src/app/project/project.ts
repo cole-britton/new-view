6487a6c76e1278891d7863043363121d83a4c1cc
Cole Britton <brxttxn@Coles-MacBook-Pro.local>
2019-04-21 14:21:03 -0700

    made projects responsive and added data

---
+++ src/app/project/project.ts
@@ -0,0 +1,6 @@
+export interface IProject {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+}
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProject } from '../project/project';
 
 @Component({
   selector: 'app-projects',
@@ -6,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+
+  imgUrlPrefix: string = "./assets/images/";
+
+  projects: IProject[] = [{
+    title: "Fairmount", description: "80 units, Slabtown Portland",
+    image: this.imgUrlPrefix + "101_Fairmount_small.gif", link: "https://youtu.be/-phCWC9yDO0"
+  }, {
+    title: "Slate", description: "75 units, Central Eastside Portland",
+    image: this.imgUrlPrefix + "803_slate_small.gif", link: "https://youtu.be/7Av-xv8MVvc"
+  }, {
+    title: "Almr", description: "57 units, Alphabet District Portland",
+    image: this.imgUrlPrefix + "416_almr_small.gif", link: "https://youtu.be/nQM-LJuPooY"
+  }, {
+    title: "Sunnyside", description: "67 units, Southeast Portland",
+    image: this.imgUrlPrefix + "314_sunnyside_small.gif", link: "https://youtu.be/CR3oo6Qi0xI"
+  }
+  ];
 
   constructor() { }
 
